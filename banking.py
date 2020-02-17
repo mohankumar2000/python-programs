df51049df7867a0b1d1d682e67350497de47a3ca
mohankumar2000 <61132787+mohankumar2000@users.noreply.github.com>
2020-02-17 15:25:43 +0530

    Add files via upload

---
+++ banking.py
@@ -0,0 +1,49 @@
+print("****Welcome to ABC Bank****")
+initial_amount=0.0
+def home():
+    n=1
+    global initial_amount
+    while(n==1):
+        print("1.Deposit  2.Withdrawal  3.Balance Enquiry  4.Quit")
+        a=int(input())
+        if(a==1):
+            deposit()
+            n=1
+        elif(a==2):
+            withdrawal()
+            n=1
+        elif(a==3):
+            print("Your account balance is:",end=" ")
+            print(initial_amount)
+            n=1
+        elif(a==4):
+            print("Thank you for using our bank")
+            n=0
+        else:
+            print("choose valid choice:")
+            n=1
+
+def deposit():
+    global initial_amount
+    print("Enter the amount to be add:")
+    d=float(input())
+    initial_amount+=d
+    print("Amount has been added successfully!!")
+    print("Your account balance is: ",initial_amount)
+
+
+def withdrawal():
+    global initial_amount
+    print("Enter the amount to be withdrawal:")
+    w=float(input())
+    if(w>initial_amount):
+        print("OOPS!! Your account does not have enough amount")
+        home()
+    else:
+        initial_amount-=w
+        print("Amount has been withdrawal successfully!!")
+        print("Your account balance is: ",initial_amount)
+
+home()
+        
+    
